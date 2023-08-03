a33a7015b4775a81698308cfa09e5f80f39f2ddd
devb <cruiseradar@gmail.com>
2023-08-03 09:26:48 +0900

    Initial commit

--- demoTest/src/main/java/com/example/demoTest/each01Controller/RestContEach01Controller.java
+++ demoTest/src/main/java/com/example/demoTest/each01Controller/RestContEach01Controller.java
@@ -1,4 +1,4 @@
-package com.example.demoTest.Controller;
+package com.example.demoTest.each01Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
